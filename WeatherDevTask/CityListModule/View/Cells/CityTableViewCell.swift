6d7de56ee0177ec9e4d75b317af462c4fb19dd64
Mikhail <>
2022-12-07 20:56:05 +0100

    create CityTableViewCell

---
+++ WeatherDevTask/CityListModule/View/Cells/CityTableViewCell.swift
@@ -0,0 +1,106 @@
+//
+//  CityTableViewCell.swift
+//  WeatherDevTask
+//
+//  Created by Mikhail on 07.12.2022.
+//
+
+import UIKit
+
+final class CityTableViewCell: UITableViewCell {
+    
+    // MARK: - Static properties
+    
+    static let reuseID = "CityTableViewCell"
+    
+    // MARK: - UI properties
+    
+    private let currentTimeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 15, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "16:07"
+        return label
+    }()
+    
+    private let cityNameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 25, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Warsaw"
+        return label
+    }()
+    
+    private let weatherImageView: UIImageView = {
+        let image = WeatherImage.sun
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let temperatureLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 50, weight: .thin)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "2°"
+        return label
+    }()
+    
+    private lazy var cityStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [currentTimeLabel,
+                                                       cityNameLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [cityStackView,
+                                                      weatherImageView,
+                                                      temperatureLabel])
+        stackView.distribution = .equalCentering
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    // MARK: - Initialization
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        backgroundColor = .black
+        addSubview(mainStackView)
+    }
+    
+    private func setConstraints() {
+        let margins = layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+            // weatherImageView
+            weatherImageView.heightAnchor.constraint(equalToConstant: 42),
+            weatherImageView.widthAnchor.constraint(equalToConstant: 42),
+            
+            //mainStackView
+            mainStackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            mainStackView.topAnchor.constraint(equalTo: margins.topAnchor),
+            mainStackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            mainStackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+        ])
+    }
+}
