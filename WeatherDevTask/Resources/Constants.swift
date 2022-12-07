8dc3f5b004338906a6ab9beb36f3bea33c256de1
Mikhail <>
2022-12-07 15:09:48 +0100

    create WeatherImage struct with static constants of weather images

---
+++ WeatherDevTask/Resources/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  WeatherDevTask
+//
+//  Created by Mikhail on 07.12.2022.
+//
+
+import UIKit
+
+struct WeatherImage {
+    static let sun = UIImage(named: "sunImage")
+    static let cloudSun = UIImage(named: "cloudSunImage")
+    static let cloud = UIImage(named: "cloudImage")
+    static let cloudRain = UIImage(named: "cloudRainImage")
+    static let cloudBolt = UIImage(named: "cloudBoltImage")
+    static let snow = UIImage(named: "snowImage")
+    static let fog = UIImage(named: "fogImage")
+}
