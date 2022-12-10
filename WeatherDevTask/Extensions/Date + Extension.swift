6cb29ce314ac0f92e2906f18a87583c39ccd0010
Mikhail <>
2022-12-10 20:35:42 +0100

    add Date + Extension

---
+++ WeatherDevTask/Extensions/Date + Extension.swift
@@ -0,0 +1,31 @@
+//
+//  Date + Extension.swift
+//  WeatherDevTask
+//
+//  Created by Mikhail on 10.12.2022.
+//
+
+import Foundation
+
+extension Date {
+    func dtToDayOfWeek(_ timezone: Int ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: timezone)
+        return dateFormatter.string(from: self)
+    }
+    
+    func dtToTime12Hours(_ timezone: Int ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "ha"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: timezone)
+        return dateFormatter.string(from: self)
+    }
+    
+    func currentTime(_ timezone: Int ) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: timezone)
+        return dateFormatter.string(from: self)
+    }
+}
