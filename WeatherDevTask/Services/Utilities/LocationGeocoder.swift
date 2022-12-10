71fa95dcdbb34e93a918ea67b9029ca20970b744
Mikhail <>
2022-12-10 17:03:34 +0100

    refactor style of "getCoordinateOf" function

--- WeatherDevTask/Services/Utilities/LocationGeocoder.swift
+++ WeatherDevTask/Services/Utilities/LocationGeocoder.swift
@@ -8,7 +8,7 @@
 import CoreLocation
 
 final class LocationGeocoder {
-    func getCoordinateOf(city: String,
+    func getCoordinateOf(_ city: String,
                          completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Error?) -> () ) {
         CLGeocoder().geocodeAddressString(city) {
             completion($0?.first?.location?.coordinate, $1)
